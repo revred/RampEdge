784543c83d4510d9d7dbc401e212ec228f50223c
Issam Boutissante <boutissante.issam.dev@gmail.com>
2025-09-24 21:09:48 +0100

    v 1.0.4

--- Maker.RampEdge/Configuration/RampEdgeSettings.cs
+++ Maker.RampEdge/Configuration/RampEdgeSettings.cs
@@ -3,7 +3,7 @@
 public class RampEdgeSettings
 {
     public const string SectionName = "RampEdge";
-    public string BaseAddress { get; set; } = string.Empty;
-    public string MakerProductBaseUrl { get; set; } = string.Empty;
+    public string BaseAddress => "https://maker-rest-api-e5c2djh7aafkace8.uksouth-01.azurewebsites.net";
+    public string MakerProductBaseUrl => "https://black-bay-067463310.2.azurestaticapps.net";
     public string BusinessUnitKey { get; set; } = string.Empty;
 }
