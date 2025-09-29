7baa5444ea52871037bafe6b2dd531c86037e637
Issam Boutissante <boutissante.issam.dev@gmail.com>
2025-09-29 08:05:52 +0100

    enabled testing configs

--- Maker.RampEdge/Configuration/RampEdgeSettings.cs
+++ Maker.RampEdge/Configuration/RampEdgeSettings.cs
@@ -3,7 +3,7 @@
 public class RampEdgeSettings
 {
     public const string SectionName = "RampEdge";
-    public string BaseAddress => "https://maker-rest-api-e5c2djh7aafkace8.uksouth-01.azurewebsites.net";
-    public string MakerProductBaseUrl => "https://black-bay-067463310.2.azurestaticapps.net";
+    public string BaseAddress { get; set; } = "https://maker-rest-api-e5c2djh7aafkace8.uksouth-01.azurewebsites.net";
+    public string MakerProductBaseUrl { get; set; } = "https://black-bay-067463310.2.azurestaticapps.net";
     public string BusinessUnitKey { get; set; } = string.Empty;
 }
